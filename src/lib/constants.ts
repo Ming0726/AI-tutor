616b1d41a048928b6c63feba5265a2a97100028a
Ming0726 <lzmhhh2024@gmail.com>
2026-03-11 11:28:34 +0100

    1

---
+++ src/lib/constants.ts
@@ -0,0 +1,14 @@
+export const EXPLAIN_STYLES = [
+  "professional",
+  "concise",
+  "humorous",
+  "friendly",
+] as const;
+
+export const KNOWLEDGE_LEVELS = [
+  "beginner",
+  "intermediate",
+  "advanced",
+] as const;
+
+export const DEFAULT_CATEGORIES = ["General", "Ergonomic", "MPD", "Statistic"];
